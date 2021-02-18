97132e30111b94bedf884d1dac9d095018a6962a
Cameron Seader <cs@suse.com>
2021-02-18 08:37:57 -0700

    remove saltboot and update config.sh

--- OSImage/POS_Image-JeOS7/images.sh
+++ OSImage/POS_Image-JeOS7/images.sh
@@ -22,10 +22,10 @@
 test -f /.kconfig && . /.kconfig
 test -f /.profile && . /.profile
 
-#systemctl enable salt-minion.service
+systemctl enable salt-minion.service
 
 # notify SUSE Manager about newly deployed image
-#systemctl enable image-deployed.service
+systemctl enable image-deployed.service
 
 # install bootloader and generate boot menu
 systemctl enable install-local-bootloader.service
